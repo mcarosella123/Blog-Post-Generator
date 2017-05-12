06636431d25e6ed28ccbab276b766f25ffffeacf
Mark Carosella <mcarosel@heidelberg.edu>
2017-05-11 21:13:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stackPost = new Post("Exercise 2", "Exercise 1 was rough.", new DateTime(2017,5,11));
+            stackPost.DownVote();
+            stackPost.DownVote();
+            stackPost.DownVote();
+            stackPost.UpVote();
+            stackPost.DownVote();
+            stackPost.UpVote();
+            stackPost.UpVote();
+            stackPost.DownVote();
+            Console.WriteLine(stackPost);
+        }
+    }
+}
